8f157ed03e77c7261187e808ee11c94d667eee7d
PruebaDev <PruebaDev@gmail.com>
2025-03-11 20:32:50 -0600

    cambios para mostar materiales

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // 🔥 Ajuste importante para el despliegue en Railway
+  server: {
+    port: 5173, // Puerto que usa Vite en desarrollo
+    host: true, // Permite que Railway acceda a la app en el puerto asignado
+  },
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos de producción
+  }
+});
