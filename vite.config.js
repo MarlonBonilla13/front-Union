a82008e6dcb3f610f5a217acd9536e931fc837bd
PruebaDev <PruebaDev@gmail.com>
2025-03-11 22:31:16 -0600

    cambios material 12

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',  // Usar ruta absoluta en lugar de './'
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Asegúrate de que la salida sea 'dist'
+    rollupOptions: {
+      input: '/index.html',  // Asegúrate de que el archivo de entrada sea el correcto
+    },
+  },
 })
