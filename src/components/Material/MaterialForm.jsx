74e262722d7f8b0f4c885798f8e86ae88708a09f
PruebaDev <PruebaDev@gmail.com>
2025-03-11 22:42:13 -0600

    cambio para materialForm por cargar

--- src/components/Material/MaterialForm.jsx
+++ src/components/Material/MaterialForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createMaterial } from "../../services/materialService";  // Asegúrate de importar el servicio correctamente
 
 const MaterialForm = () => {
