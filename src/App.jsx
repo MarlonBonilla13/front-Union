5c6306422ac8c477cc7d8a0ea327010b7a7cb24c
PruebaDev <PruebaDev@gmail.com>
2025-03-11 20:19:31 -0600

    Redirigir a materiales por defecto"

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
-import { useEffect, useState } from 'react';
-import api from './services/api'; // Asegúrate de que la ruta sea correcta// Asegúrate de que esta ruta sea correcta
-import MaterialForm from './components/Material/MaterialForm.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import MaterialForm from "./components/Material/MaterialForm.jsx";
 
 function App() {
-  const [data, setData] = useState([]); // Estado para los materiales
-
-  useEffect(() => {
-    // Cargar los materiales desde la API
-    api.get('/materiales') // Ajusta la URL si es necesario
-      .then((res) => setData(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  // Función para manejar la creación de un nuevo material
-  const handleMaterialCreated = (newMaterial) => {
-    setData((prevData) => [newMaterial, ...prevData]); // Agregar el nuevo material a la lista
-  };
-
   return (
-    <>
-      <h1>Materiales</h1>
-      <MaterialForm onMaterialCreated={handleMaterialCreated} />
-
-      <h2>Lista de Materiales:</h2>
-      <ul>
-        {data.length === 0 ? (
-          <p>No hay materiales disponibles.</p>
-        ) : (
-          data.map((material) => (
-            <li key={material.id_material}>
-              {material.nombre} - {material.precio_unitario} - {material.estado ? 'Activo' : 'Inactivo'}
-            </li>
-          ))
-        )}
-      </ul>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/materiales" />} />
+        <Route path="/materiales" element={<MaterialForm />} />
+      </Routes>
+    </Router>
   );
 }
 
