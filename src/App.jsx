54d184f8c9b06af3f0eab7bdd70f5f9f19f069be
PruebaDev <PruebaDev@gmail.com>
2025-03-11 21:11:39 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MaterialForm from './components/Material/MaterialForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Página principal */}
         <Route path="/material" element={<MaterialForm />} />  {/* Ruta para MaterialForm */}
       </Routes>
     </Router>
